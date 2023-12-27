3d47e5cb2bbfbc97ddc8d4e5dfab7838978a87c5
Cédric Lampron <cedric_lampron@hotmail.com>
2023-12-27 09:18:17 -0500

    Hide from alt tab

--- src/Hunt.Lurker/Views/ShellView.xaml.cs
+++ src/Hunt.Lurker/Views/ShellView.xaml.cs
@@ -7,8 +7,29 @@ namespace Hunt.Lurker.Views;
 /// </summary>
 public partial class ShellView : Window
 {
+    private Window _parent;
+
     public ShellView()
     {
         InitializeComponent();
+        HideFromAltTab();
+    }
+
+    private void HideFromAltTab()
+    {
+        _parent = new Window
+        {
+            Top = -100,
+            Left = -100,
+            Width = 1,
+            Height = 1,
+
+            WindowStyle = WindowStyle.ToolWindow, // Set window style as ToolWindow to avoid its icon in AltTab
+            ShowInTaskbar = false,
+        };
+
+        _parent.Show();
+        Owner = _parent;
+        _parent.Hide();
     }
 }
